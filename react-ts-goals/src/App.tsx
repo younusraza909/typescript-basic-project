496963c64bd05f7a10385c99a5ab898d0a2c5d06
younusraza909 <younusraza909@gmail.com>
2024-02-22 15:27:57 +0500

    basic to do app completed

--- react-ts-goals/src/App.tsx
+++ react-ts-goals/src/App.tsx
@@ -1,24 +1,17 @@
-import CourseGoal from "./components/CourseGoal";
 import Header from "./components/Header";
 
 import goalImage from "./assets/goals.jpg";
 import { useState } from "react";
 import CourseGoalList from "./components/CourseGoalList";
 import { Goal } from "./types";
-import NewGoal from "./components/newGoal";
+import NewGoal from "./components/NewGoal";
 
 export default function App() {
   const [goals, setGoals] = useState<Goal[]>([]);
 
-  function handleAddGoal() {
-    setGoals((prev) => [
-      ...prev,
-      {
-        description: "Learn from ground up",
-        title: "Learn React + TS",
-        id: Math.random(),
-      },
-    ]);
+  function handleAddGoal(goal: string, summary: string) {
+    const newGoal = { title: goal, description: summary, id: Math.random() };
+    setGoals((prev) => [...prev, newGoal]);
   }
 
   function handleDeleteGoal(id: number) {
@@ -30,7 +23,7 @@ export default function App() {
       <Header image={{ src: goalImage, alt: "A list of goals" }}>
         <h1>Yout Course Goals</h1>
       </Header>
-      <NewGoal />
+      <NewGoal onAddGoal={handleAddGoal} />
 
       <CourseGoalList goals={goals} onDelete={handleDeleteGoal} />
     </main>
--- react-ts-goals/src/components/NewGoal.tsx
+++ react-ts-goals/src/components/NewGoal.tsx
@@ -1,23 +1,37 @@
-import { type FormEvent } from "react";
+import { useRef, type FormEvent } from "react";
 
-const NewGoal = () => {
+const NewGoal = ({
+  onAddGoal,
+}: {
+  onAddGoal: (goal: string, summary: string) => void;
+}) => {
+  const goal = useRef<HTMLInputElement>(null);
+  const summary = useRef<HTMLInputElement>(null);
+
   function onSubmitForm(event: FormEvent<HTMLFormElement>) {
     // now in order to get form values we have 3 options
     // 1) Data binding with state
     // 2) using formData with current Target
     // const formData = new FormData(event.currentTarget);
     //3) Using Ref
+    event.preventDefault();
+    const inputGoal = goal.current!.value;
+    const inputSummary = summary.current!.value;
+
+    event.currentTarget.reset();
+
+    onAddGoal(inputGoal, inputSummary);
   }
 
   return (
     <form onSubmit={onSubmitForm}>
       <p>
         <label htmlFor="goal">Enter your goal</label>
-        <input type="text" name="goal" id="goal" />
+        <input type="text" name="goal" id="goal" ref={goal} />
       </p>
       <p>
         <label htmlFor="summary">Give a short summary</label>
-        <input type="text" name="summary" id="summary" />
+        <input type="text" name="summary" id="summary" ref={summary} />
       </p>
       <p>
         <button type="submit">Add Goal</button>
