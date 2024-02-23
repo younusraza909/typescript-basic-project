15ed232ca5ef5f5345e3bbfde99fc4da7875ad97
younusraza909 <younusraza909@gmail.com>
2024-02-23 16:41:06 +0500

    Polymorphic component

--- adv-components-react/src/App.tsx
+++ adv-components-react/src/App.tsx
@@ -1,4 +1,5 @@
 import Button from "./components/Button";
+import Container from "./components/Container";
 import Input from "./components/Input";
 
 function App() {
@@ -7,6 +8,8 @@ function App() {
       <h1>Let's get started!</h1>
       <Input id="name" label="Enter your name" type="text" />
       <Input id="email" label="Enter your email" type="email" />
+
+      <Container as={Button}>Button</Container>
     </>
   );
 }
