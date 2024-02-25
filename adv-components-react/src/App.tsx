32372d386125da27f5a760ba4925b2386afd77ed
younusraza909 <younusraza909@gmail.com>
2024-02-25 15:46:13 +0500

    making dynamic form components

--- adv-components-react/src/App.tsx
+++ adv-components-react/src/App.tsx
@@ -1,16 +1,33 @@
+import { useRef } from "react";
 import Button from "./components/Button";
 import Container from "./components/Container";
+import Form, { imperativeType } from "./components/Form";
 import Input from "./components/Input";
 
+type formInput = {
+  name: string;
+  email: string;
+};
+
 function App() {
+  const formRef = useRef<imperativeType>(null);
+
+  function onFormSave(data: any) {
+    const formData = data as formInput;
+
+    console.log(formData);
+    formRef.current?.clear();
+  }
+
   return (
-    <>
+    <Form onSave={onFormSave} ref={formRef}>
       <h1>Let's get started!</h1>
       <Input id="name" label="Enter your name" type="text" />
       <Input id="email" label="Enter your email" type="email" />
-
-      <Container as={Button}>Button</Container>
-    </>
+      <p>
+        <Button>Submit</Button>
+      </p>
+    </Form>
   );
 }
 
--- adv-components-react/src/components/Input.tsx
+++ adv-components-react/src/components/Input.tsx
@@ -15,7 +15,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
     return (
       <p>
         <label htmlFor={id}>{label}</label>
-        <input id={id} {...props} ref={ref} />
+        <input id={id} {...props} ref={ref} name={id} />
       </p>
     );
   }
