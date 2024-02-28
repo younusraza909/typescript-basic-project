04067bdb046f7f6a6550b805ff05b45159904215
younusraza909 <younusraza909@gmail.com>
2024-02-28 15:27:26 +0500

    added a button component with dynamic rendering

--- assignment/src/components/Header.tsx
+++ assignment/src/components/Header.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
 import Button from "./ui/Button";
 
 const Header = () => {
   return (
-    <div>
-      <Button />
-    </div>
+    <header id="main-header">
+      <p>ReactMentoring</p>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : "")}
+              end
+            >
+              Our Mission
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/sessions"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Browse Sessions
+            </NavLink>
+          </li>
+          <li>
+            <Button>Upcoming Sessions</Button>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
--- assignment/src/components/ui/Button.tsx
+++ assignment/src/components/ui/Button.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { type ComponentPropsWithoutRef } from "react";
+import { Link, type LinkProps } from "react-router-dom";
 
-const Button = () => {
-  return <div>Button</div>;
+type BaseProps = {
+  children: React.ReactNode;
+  textOnly?: boolean;
+};
+
+type ButtonProps = ComponentPropsWithoutRef<"button"> &
+  BaseProps & {
+    to?: never;
+  };
+
+type ButtonLinkProps = LinkProps &
+  BaseProps & {
+    to?: string;
+  };
+
+function isButtonLinkProps(
+  props: ButtonProps | ButtonLinkProps
+): props is ButtonLinkProps {
+  return "to" in props;
+}
+
+const Button = (props: ButtonProps | ButtonLinkProps) => {
+  if (isButtonLinkProps(props)) {
+    const { children, textOnly, ...otherProps } = props;
+    return (
+      <Link
+        {...props}
+        className={`button ${textOnly ? "button--text-only" : ""}`}
+      >
+        {children}
+      </Link>
+    );
+  }
+
+  const { children, textOnly, ...otherProps } = props;
+  return (
+    <button
+      {...otherProps}
+      className={`button ${textOnly ? "button--text-only" : ""}`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
