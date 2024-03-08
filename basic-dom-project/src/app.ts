fc64d827b88554578a69b601c71bdc82877ccdf7
younusraza909 <younusraza909@gmail.com>
2024-03-08 11:49:26 +0500

    Project Manager DOM Project :gathering user information

--- basic-dom-project/src/app.ts
+++ basic-dom-project/src/app.ts
@@ -1,5 +1,44 @@
 // Code goes here!
 
+interface validatableData {
+  value?: string | number;
+  required?: boolean;
+  minLenght?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(data: validatableData): boolean {
+  let isValid = true;
+
+  if (data.required) {
+    isValid = isValid && data.value?.toString().trim().length !== 0;
+  }
+
+  if (!data.required && data.value?.toString().trim().length === 0) {
+    return isValid;
+  }
+
+  if (data.minLenght && typeof data.value === "string") {
+    isValid = isValid && data.value.length >= data.minLenght;
+  }
+
+  if (data.min && typeof data.value === "number") {
+    isValid = isValid && data.value >= data.min;
+  }
+
+  if (data.max && typeof data.value === "number") {
+    isValid = isValid && data.value <= data.max;
+  }
+
+  if (data.maxLength && typeof data.value === "string") {
+    isValid = isValid && data.value.length >= data.maxLength;
+  }
+
+  return isValid;
+}
+
 function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
 
@@ -53,9 +92,31 @@ class ProjectInput {
     this.mountUi();
   }
 
+  // we are using decorator which auto bind this keyword here
   @AutoBind
   private handleFormSubmission(event: Event) {
     event.preventDefault();
+    this.gatherUserInformation();
+  }
+
+  private gatherUserInformation(): [string, string, number] | void {
+    const titleInputValue = this.titleInput.value;
+    const descriptionInputValue = this.descriptionInput.value;
+    const peopleInputValue = this.peopleInput.value;
+
+    if (
+      !validate({ value: titleInputValue, required: false, minLenght: 3 }) ||
+      !validate({
+        value: descriptionInputValue,
+        required: true,
+        minLenght: 3,
+      }) ||
+      !validate({ value: peopleInputValue, required: true, min: 0 })
+    ) {
+      alert("Invalid Input Data, kindly recheck it");
+    } else {
+      return [titleInputValue, descriptionInputValue, +peopleInputValue];
+    }
   }
 
   private authorize() {
